89ca3d220f7ce7f772a03debc839f0fade39e207
oroafrica <52736386+oroafrica@users.noreply.github.com>
2021-02-26 13:31:00 +0200

    add onchange

--- inputFonts.js
+++ inputFonts.js
@@ -1,1 +1,6 @@
-(()=>{$("input[type='text']").css("font-family","'z_swan'");})();
+(()=>
+ { 
+  let a = $("input[type='text']");
+  $(document).on("onchange",$(a),()=>{$("input[type='text']").css("font-family","'z_swan'")});
+  //$("input[type='text']").css("font-family","'z_swan'")
+ })();
