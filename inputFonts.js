572c72fe7929eccf4a40b6e1b3b868b9ba1cf6d6
oroafrica <52736386+oroafrica@users.noreply.github.com>
2021-02-26 13:34:58 +0200

    add color

--- inputFonts.js
+++ inputFonts.js
@@ -1,6 +1,4 @@
 (()=>
  { 
-  let a = $("input[type='text']");
-  $(document).on("onchange",$(a),()=>{$("input[type='text']").css("font-family","'z_swan'")});
-  //$("input[type='text']").css("font-family","'z_swan'")
+  $("input[type='text']").css({"font-family":"'z_swan'","color":"#00f"})
  })();
